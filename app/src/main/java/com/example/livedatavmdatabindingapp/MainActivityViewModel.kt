81c3067664775996599eed67ee6d49a3e103af60
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-03-13 18:57:20 +0300

    First commit

---
+++ app/src/main/java/com/example/livedatavmdatabindingapp/MainActivityViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.livedatavmdatabindingapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+    private var counter = MutableLiveData<Int>()
+    val countData: LiveData<Int>
+        get() = counter
+
+
+    init{
+        counter.value = 0
+    }
+
+    fun updateCounter(){
+        counter.value=(counter.value)?.plus(1)
+    }
+}
